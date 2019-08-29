c9a17404345849994ad5e70c61bac6eb3c263c8b
Chantel Lowe <chantellowe17@gmail.com>
2019-08-29 04:52:16 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,6 +1,6 @@
 class CashRegister
   
-  attr_accessor :total, :discount, :last_transaction, :items
+  attr_accessor :total, :discount, :last_transaction, :items, :price
   
   def initialize(discount = 0)
     @total = 0
