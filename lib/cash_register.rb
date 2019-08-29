db8bca6f2bcc660ca60840cce07ff95a92272799
Chantel Lowe <chantellowe17@gmail.com>
2019-08-29 00:17:15 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -2,8 +2,8 @@ class CashRegister
   
   attr_accessor :total, :discount
   
-  def initialize(discount, total = 0)
-    @total = total
+  def initialize(discount = 0)
+    @total = 0
     @discount = discount
   end
   
