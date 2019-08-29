62c0efe6a2b917507ca3bec7cbdf0630e140b361
Chantel Lowe <chantellowe17@gmail.com>
2019-08-29 04:33:02 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -14,8 +14,9 @@ class CashRegister
     if quantity > 1 
       counter = 0 
       while counter < quantity
-      @items << title
-      counter += 1 
+        @items << title
+        counter += 1 
+      end
     else
       @items << title 
     end
